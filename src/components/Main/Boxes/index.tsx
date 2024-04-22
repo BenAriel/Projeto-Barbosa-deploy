e2d838d56fe1bc50c3d5e56bcaf89dd623f0d817
BenAriel <fadomarshemello@gmail.com>
2024-04-21 22:24:01 -0300

    retornando o json,porém não totalmente perfeito

--- src/components/Main/Boxes/index.tsx
+++ src/components/Main/Boxes/index.tsx
@@ -1,58 +1,61 @@
 import React from "react";
 
 interface BoxesProps {
-    handleInputChange: (input: string) => void;
-    inputValue: string;
-    chatGPTResponse: string | null; // Resposta do ChatGPT
+  handleInputChange: (input: string) => void;
+  inputValue: string;
+  chatGPTResponse: string | null;
 }
 
-const InputBox: React.FC<Pick<BoxesProps, "handleInputChange" | "inputValue">> = ({ handleInputChange, inputValue }) => {
-    const handleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
-        handleInputChange(event.target.value);
-    };
+const InputBox: React.FC<Pick<BoxesProps, "handleInputChange" | "inputValue">> = ({
+  handleInputChange,
+  inputValue,
+}) => {
+  const handleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    handleInputChange(event.target.value);
+  };
 
-    return (
-        <div className="w-[75%] lg:w-1/2 h-[100%] border-2 border-gray-700 mx-8 bg-gray-200">
-            <div className="w-full h-[15%] md:h-[10%] lg:h-[10%] border-b-2 border-gray-700 flex justify-center items-center font-bold text-xl bg-white">
-                Digite seu Texto:
-            </div>
-            <div className="w-full h-[85%] lg:h-[90%] bg-gray-200">
-                <textarea
-                    id="entrada"
-                    className="w-full h-full border-none outline-none text-xl resize-none bg-gray-200 px-1"
-                    value={inputValue}
-                    onChange={handleChange}
-                    placeholder="Digite seu texto"
-                ></textarea>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-[75%] lg:w-1/2 h-[100%] border-2 border-gray-700 mx-8 bg-gray-200">
+      <div className="w-full h-[15%] md:h-[10%] lg:h-[10%] border-b-2 border-gray-700 flex justify-center items-center font-bold text-xl bg-white">
+        Digite seu Texto:
+      </div>
+      <div className="w-full h-[85%] lg:h-[90%] bg-gray-200">
+        <textarea
+          id="entrada"
+          className="w-full h-full border-none outline-none text-xl resize-none bg-gray-200 px-1"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Digite seu texto"
+        ></textarea>
+      </div>
+    </div>
+  );
 };
 
 const OutPutBox: React.FC<Pick<BoxesProps, "chatGPTResponse">> = ({ chatGPTResponse }) => {
-    return (
-        <div className="w-[75%] lg:w-1/2 h-[100%] border-2 border-gray-700 mx-8 bg-gray-200">
-            <div className="w-full h-[15%] md:h-[10%] lg:h-[10%] border-b-2 border-gray-700 flex justify-center items-center font-bold text-xl bg-white">
-                Correção Sugerida:
-            </div>
-            <div className="w-full placeholder-dinamico h-[85%] lg:h-[90%] text-xl px-1">
-                {chatGPTResponse !== null ? (
-                    <span>{chatGPTResponse}</span>
-                ) : (
-                    <span>Nada.</span>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-[75%] lg:w-1/2 h-[100%] border-2 border-gray-700 mx-8 bg-gray-200">
+      <div className="w-full h-[15%] md:h-[10%] lg:h-[10%] border-b-2 border-gray-700 flex justify-center items-center font-bold text-xl bg-white">
+        Correção Sugerida:
+      </div>
+      <div className="w-full placeholder-dinamico h-[85%] lg:h-[90%] text-xl px-1">
+        {chatGPTResponse !== null ? (
+          <span>{chatGPTResponse}</span>
+        ) : (
+          <span>Nada.</span>
+        )}
+      </div>
+    </div>
+  );
 };
 
 const Boxes: React.FC<BoxesProps> = ({ handleInputChange, inputValue, chatGPTResponse }) => {
-    return (
-        <div className="flex flex-col md:flex-row items-center gap-y-10 lg:gap-y-0 h-[90%] w-full">
-            <InputBox handleInputChange={handleInputChange} inputValue={inputValue} />
-            <OutPutBox chatGPTResponse={chatGPTResponse} />
-        </div>
-    );
+  return (
+    <div className="flex flex-col md:flex-row items-center gap-y-10 lg:gap-y-0 h-[90%] w-full">
+      <InputBox handleInputChange={handleInputChange} inputValue={inputValue} />
+      <OutPutBox chatGPTResponse={chatGPTResponse} />
+    </div>
+  );
 };
 
 export default Boxes;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,69 +1,82 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Boxes from "./Boxes";
 
 const Main = () => {
-    const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
 
-    const handleInputChange = (input: string) => {
-        setInputValue(input);
-    };
+  const handleInputChange = (input: string) => {
+    setInputValue(input);
+  };
 
-    const handleCorrection = async () => {
-        const texto = inputValue.trim();
+  const handleCorrection = async () => {
+    const texto = inputValue.trim();
 
-        if (texto !== "") {
-            const requestBody = {
-                messages: [
-                    {
-                        role: "system",
-                        content: "Você é um assistente de correção de texto, identifique erros gramaticais, sugira reorganização de ideias e adaptações para tornar o texto mais compreensível. Retorne, além do texto original e corrigido, um objeto JSON com todas as palavras que foram corrigidas, cada palavra seguida de sua correção.",
-                    },
-                    { role: "user", content: texto },
-                ],
-                model: "gpt-3.5-turbo",
-                response_format: { type: "json_object" },
-            };
+    if (texto !== "") {
+      const seed = 1;
+      const temperatura = 0.01; 
+      const requestBody = {
+        messages: [
+          {
+            role: "system",
+            content: "Você é um assistente de correção de texto, identifique erros gramaticais, sugira reorganização de ideias e adaptações para tornar o texto mais compreensível. Retorne, além do texto original e corrigido, um objeto JSON com todas as palavras que foram corrijidas, cada palavra seguida de sua correção.",
+          },
+          { role: "user", content: texto },
+        ],
+        model: "gpt-3.5-turbo",
+        response_format: { type: "json_object" },
+        temperature: temperatura,
+         seed: seed,
+      };
 
-            const requestOptions = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer sk-proj-zz1mViaJ44kziGXL0YjXT3BlbkFJ4XDuuwonG9veLXOBNBvz'
-                },
-                body: JSON.stringify(requestBody)
-            };
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer sk-proj-ZRyqX0aGqI0eDw7K95baT3BlbkFJRWEdJDOBza4KUxcJ7h0x",
+        },
+        body: JSON.stringify(requestBody),
+      };
 
-            try {
-                const response = await fetch('https://api.openai.com/v1/chat/completions', requestOptions);
-                const data = await response.json();
-                const chatGPTResponse = data.choices[0]?.message?.content;
-                console.log("Resposta do ChatGPT:", chatGPTResponse);
-                // Aqui você pode atualizar o estado ou fazer algo com a resposta do ChatGPT
-            } catch (error) {
-                console.error("Erro ao obter resposta do ChatGPT:", error);
-            }
-        }
-    };
+      try {
+        const response = await fetch(
+          "https://api.openai.com/v1/chat/completions",
+          requestOptions
+        );
+        const data = await response.json();
+        const chatGPTResponse = data.choices[0]?.message?.content;
+        console.log("Resposta do ChatGPT:", chatGPTResponse);
 
-    return (
-        <div className="w-full h-[90%] flex flex-row flex-wrap">
-            <div className="w-full h-[90%]">
-                <Boxes
-                    handleInputChange={handleInputChange}
-                    inputValue={inputValue}
-                    chatGPTResponse={null}
-                />
-            </div>
-            <div className="w-full flex justify-center mt-4">
-                <button
-                    onClick={handleCorrection}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                    Corrigir Texto
-                </button>
-            </div>
-        </div>
-    );
+        // Atualizar o estado com a resposta da API
+        setChatGPTResponse(chatGPTResponse);
+
+      } catch (error) {
+        console.error("Erro ao obter resposta do ChatGPT:", error);
+      }
+    }
+  };
+
+  const [chatGPTResponse, setChatGPTResponse] = useState<string | null>(null);
+
+  return (
+    <div className="w-full h-[90%] flex flex-row flex-wrap">
+      <div className="w-full h-[90%]">
+        <Boxes
+          handleInputChange={handleInputChange}
+          inputValue={inputValue}
+          chatGPTResponse={chatGPTResponse}
+        />
+      </div>
+      <div className="w-full flex justify-center mt-4">
+        <button
+          onClick={handleCorrection}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Corrigir Texto
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
